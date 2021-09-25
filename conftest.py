bf883d000cbee1486f5dd54d29130d7043bd857f
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-09-26 02:28:10 +0300

    Настройка протокола FTP и отключение капчи для автотестов

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import json
 import os.path
 
+import ftputil
 import pytest
 
 from fixture.application import Application
@@ -10,12 +11,17 @@ fixture = None
 target = None
 
 
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
 @pytest.fixture()
-def app(request):
+def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))["web"]
-    web_admin_config = load_config(request.config.getoption("--target"))["web_admin"]
+    web_config = config["web"]
+    web_admin_config = config["web_admin"]
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config["base_url"])
     fixture.session.ensure_login(web_admin_config["username"], web_admin_config["password"])
@@ -32,14 +38,9 @@ def stop(request):
     return fixture
 
 
-def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--target", action="store", default="target.json")
-
-
 @pytest.fixture(scope="session")
-def db(request):
-    db_config = load_config(request.config.getoption("--target"))["db"]
+def db(request, config):
+    db_config = config["db"]
     db_fixture = DbFixture(host_value=db_config["host"], name_value=db_config["name"], user_value=db_config["user"],
                            password_value=db_config["password"])
 
@@ -48,6 +49,39 @@ def db(request):
 
     request.addfinalizer(final)
     return db_fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    ftp_config = config['ftp']
+    install_server_configuration(ftp_config['host'], ftp_config['username'], ftp_config['password'])
+
+    def final():
+        restore_server_configuration(ftp_config['host'], ftp_config['username'], ftp_config['password'])
+
+    request.addfinalizer(final)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.back"):
+            remote.remove("config_inc.php.back")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.back")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.back"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.back", "config_inc.php")
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
 
 
 def load_config(file):
--- test/test_login.py
+++ test/test_login.py
@@ -1,3 +1,4 @@
 def test_login(app):
-    app.session.login("administrator", "root")
+    # Executing automatically by ensure_login function
+    # app.session.login("administrator", "root")
     assert app.session.is_logged_in_as("administrator")
